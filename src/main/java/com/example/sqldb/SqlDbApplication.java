de9059b78c84db81e817b9db9a193921a35fea87
권길남 <nimpa3201@naver.com>
2023-05-24 14:27:12 +0900

    commit message

---
+++ src/main/java/com/example/sqldb/SqlDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqldb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqldb/SqlDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
